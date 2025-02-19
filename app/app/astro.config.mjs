f0d8e94c57af468b03bfd1a8fb62f35eedd61077
Matiboux <matiboux@gmail.com>
2025-02-19 20:40:53 +0100

    Update packages & build config

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
-		server: {
-			watch: {
-				usePolling: true,
-			},
-		},
+		plugins: [
+			tailwindcss(),
+		],
 	},
-	integrations: [
-		tailwind(),
-	],
 })
