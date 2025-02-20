4823af010efc456982426cd462d91e3267d7e007
Matiboux <matiboux@gmail.com>
2025-02-21 00:04:37 +0100

    Add i18n config

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
+import { i18n } from '/src/config'
+
 // https://astro.build/config
 export default defineConfig({
+	i18n: i18n,
 	vite: {
 		plugins: [
 			tailwindcss(),
